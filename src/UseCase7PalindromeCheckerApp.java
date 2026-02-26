ea41e54422010fec7f4284e6729f3e6fe9b03b0a
Swastik011005 <swastikbasuu2005@gmail.com>
2026-02-26 10:58:11 +0530

    UC7: Deque Based Optimized Palindrome Checker

---
+++ src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class UseCase7PalindromeCheckerApp {
+
+    /**
+     * Application entry point for UC7.
+     */
+    public static void main(String[] args) {
+
+        String input = "madam";
+
+        Deque<Character> deque = new LinkedList<>();
+
+        // Insert characters into deque
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare from both ends
+        while (deque.size() > 1) {
+
+            char first = deque.removeFirst();
+            char last = deque.removeLast();
+
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
+    }
+}
